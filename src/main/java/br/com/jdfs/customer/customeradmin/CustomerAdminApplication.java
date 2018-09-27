d8420e14ff7a2fa4d94b997de02eab4f7a6350a7
jacques.sa@outlook.com <jacques.sa@outlook.com>
2018-09-26 22:56:30 -0300

    Criação inicial do projeto

---
+++ src/main/java/br/com/jdfs/customer/customeradmin/CustomerAdminApplication.java
@@ -0,0 +1,12 @@
+package br.com.jdfs.customer.customeradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAdminApplication.class, args);
+	}
+}
